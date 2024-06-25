4c9a46bd027bca01877d3f591f655411e0330702
mohdaamil120 <mohdaamil120@gmail.com>
2024-06-25 18:45:01 +0530

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,125 +1,469 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Pic from "../image/hp-player-crop.webp"
-import Elf from "../image/2222.png"
-import Int from "../image/3.png"
-import Go from "../image/4.png"
-import Ro from "../image/5.png"
-import Gar from "../image/6.png"
-import Bl from "../image/7.png"
-import St from "../image/8.png"
-import Gy from "../image/9.png"
-import Ch from "../image/10.png"
-import PopupForm from './PopupForm'
+// import React, { useState } from 'react'
+// import styled from 'styled-components'
+// import Pic from "../image/hp-player-crop.webp"
+// import Elf from "../image/2222.png"
+// import Int from "../image/3.png"
+// import Go from "../image/4.png"
+// import Ro from "../image/5.png"
+// import Gar from "../image/6.png"
+// import Bl from "../image/7.png"
+// import St from "../image/8.png"
+// import Gy from "../image/9.png"
+// import Ch from "../image/10.png"
+// import PopupForm from './PopupForm'
+
+// function Home() {
+//     const [isFormVisible, setFormVisible] = useState(false);
+
+//     const handleConnectWithSalesClick = () => {
+//         setFormVisible(true);
+//       };
+    
+//       const closeForm = () => {
+//         setFormVisible(false);
+//       };
+
+//   return (
+//     <DIV>
+    
+//       <header>
+//         <div className='left'>
+//             <nav>
+//                 <span>braze</span>
+//                 <p>How it Works</p>
+//                 <p>Product</p>
+//                 <p>Solutions </p>
+//                 <p>Customers</p>
+//                 <p>Success</p>
+//                 <p>Partners</p>
+//                 <p>Resources</p>
+//                 <p>Company</p>
+//             </nav>
+//         </div>
+//         <div className='right'>
+//             <select name="" id="">
+//                 <option value="us"> US</option>
+//             </select>
+//             <button id='login'>Login</button>
+//             <button id='get_start'>Get Started</button>
+//         </div>
+//       </header>
+
+//     {/* header end */}
+    
+//       <div className='main'>
+//         <div className='text_con'>
+//             <h1>There's never been a better time to be a better marker.<span>TM</span></h1>
+//             <p>Data is flowing and channels are growing. Customers are demanding the world, while AI already transfroming it. Manage it all with the right <span>customer engagement platform.</span></p>
+//             <div className='btns'>
+//                 <button>Get Started</button>
+//                 <button>Watch Video</button>
+//             </div>
+//         </div>
+//         <div className='image_con'>
+//             <img src={Pic} alt="" />
+//         </div>
+//       </div>
+      
+//       {/* main end */}
+
+//       <div className='banner'>
+//         <h3>MADE TO SCALE WITH COMPANIES OF ALL SIZES</h3>
+//         <div className='icon_con'>
+//             <div className='icons'>
+//                 <img src={Elf} alt="" />
+//                 <img src={Int} alt="" />
+//                 <img src={Go} alt="" />
+//                 <img src={Ro} alt="" />
+//                 <img src={Gar} alt="" />
+//                 <img src={Bl} alt="" />
+//                 <img src={St} alt="" />
+//                 <img src={Gy} alt="" />
+//                 <img src={Ch} alt="" />
+
+//                 {/* Duplicate set of images */}
+                
+//                 <img src={Elf} alt="" />
+//                 <img src={Int} alt="" />
+//                 <img src={Go} alt="" />
+//                 <img src={Ro} alt="" />
+//                 <img src={Gar} alt="" />
+//                 <img src={Bl} alt="" />
+//                 <img src={St} alt="" />
+//                 <img src={Gy} alt="" />
+//                 <img src={Ch} alt="" />
+//             </div>
+            
+//         </div>
+//         <button className='connect' onClick={handleConnectWithSalesClick}>
+//             Connect With Sales
+//         </button>
+//       </div>
+      
+
+//       {isFormVisible && <PopupForm closeForm={closeForm} />}
+
+//     </DIV>
+//   )
+// }
+
+// export default Home
+
+
+// const DIV = styled.div`
+//   header {
+//     width: 80%;
+//     background-color: white;
+//     border-radius: 50px;
+//     margin: auto; 
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 1rem; 
+//     position: relative;
+//     top: 32px;
+//   }
+
+//   nav {
+//     display: flex;
+//     justify-content: space-evenly;
+//     align-items: center;
+//     gap: 1rem; 
+//     font-weight: bolder;
+//   }
+
+//   p {
+//     font-size: 0.75rem; 
+//   }
+
+//   nav > span {
+//     font-family: Cedarville Cursive;
+//     font-size: 1.5rem; 
+//   }
+
+//   select {
+//     margin-right: 0.5rem; 
+//     background-color: whitesmoke;
+//   }
+
+//   #login {
+//     margin-right: 0.5rem; 
+//     background: none;
+//     border: none;
+//     font-weight: bold;
+//     cursor: pointer;
+//   }
+
+//   #get_start {
+//     margin-right: 0.5rem; 
+//     border-radius: 50px;
+//     border: none;
+//     padding: 0.75rem 1rem; 
+//     background-color: #4E0BE0;
+//     color: white;
+//     font-weight: bold;
+//     cursor: pointer;
+//   }
+
+//   .main {
+//     display: flex;
+//     width: 80%;
+//     margin: 2rem auto; 
+//     color: white;
+//     min-height: 55vh;
+//   }
+
+//   .text_con {
+//     width: 50%;
+//     display: flex;
+//     flex-direction: column;
+//     gap: 1rem; 
+//     padding-top: 2rem; 
+//   }
+
+//   .text_con > h1 {
+//     font-size: 3rem;
+//   }
+
+//   .text_con > h1 > span {
+//     font-size: 0.5rem; 
+//     position: relative;
+//     top: -0.5rem; 
+//   }
+
+//   .text_con > p {
+//     font-size: 1rem; 
+//     color: #e7dede;
+//     margin-top: -1rem; 
+//   }
+
+//   .text_con > p > span {
+//     color: white;
+//     font-weight: bold;
+//   }
+
+//   .image_con {
+//     width: 50%;
+//   }
+
+//   .image_con > img {
+//     width: 100%;
+//     max-width: 550px;
+  
+//     height: auto; 
+//   }
+
+//   .btns {
+//     display: flex;
+//     gap: 1rem; 
+//   }
+
+//   .btns > button:nth-child(1) {
+//     border-radius: 50px;
+//     border: none;
+//     padding: 0.75rem 1rem; 
+//     background-color: white;
+//     color: #8136CF;
+//     font-weight: bold;
+//     cursor: pointer;
+//   }
+
+//   .btns > button:nth-child(2) {
+//     border: none;
+//     background: transparent;
+//     padding: 0.75rem 1rem; 
+//     color: white;
+//     font-weight: bold;
+//     cursor: pointer;
+//   }
+
+//   .banner {
+//     background-color: white;
+//     border-radius: 70px 70px 0 0;
+//     text-align: center;
+//     padding-top: 1rem; 
+//     min-height: 220px;
+//     margin-top: -35px;
+   
+
+//   }
+
+//   .banner > h3 {
+//     color: gray;
+//     margin-bottom: 2rem; 
+//   }
+
+//   .icon_con {
+//     width: 80%;
+//     margin: auto;
+//     display: flex;
+//     overflow: hidden;
+//   }
+
+//   .icons {
+//     display: flex;
+//     gap: 0 2rem; 
+//     animation: slide 18s linear infinite;
+//   }
+
+//   @keyframes slide {
+//     0% {
+//       transform: translateX(0);
+//     }
+//     100% {
+//       transform: translateX(-50%);
+//     }
+//   }
+
+//   .connect {
+//     position: relative;
+//     top: -2.5rem; 
+//     left: 47%; 
+//     transform: translateX(-50%); 
+//     width: auto;
+//     max-width: 250px;
+//     border-radius: 50px;
+//     border: none;
+//     padding: 0.75rem 1rem; 
+//     background-color: #4E0BE0;
+//     color: white;
+//     font-weight: bold;
+//     cursor: pointer;
+//   }
+// `;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////
+
+
+
+
+
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Pic from '../image/hp-player-crop.webp';
+import Elf from '../image/2222.png';
+import Int from '../image/3.png';
+import Go from '../image/4.png';
+import Ro from '../image/5.png';
+import Gar from '../image/6.png';
+import Bl from '../image/7.png';
+import St from '../image/8.png';
+import Gy from '../image/9.png';
+import Ch from '../image/10.png';
+import PopupForm from './PopupForm';
 
 function Home() {
-    const [isFormVisible, setFormVisible] = useState(false);
+  const [isFormVisible, setFormVisible] = useState(false);
 
-    const handleConnectWithSalesClick = () => {
-        setFormVisible(true);
-      };
-    
-      const closeForm = () => {
-        setFormVisible(false);
-      };
+  const handleConnectWithSalesClick = () => {
+    setFormVisible(true);
+  };
+
+  const closeForm = () => {
+    setFormVisible(false);
+  };
 
   return (
     <DIV>
-    
       <header>
         <div className='left'>
-            <nav>
-                <span>braze</span>
-                <p>How it Works</p>
-                <p>Product</p>
-                <p>Solutions </p>
-                <p>Customers</p>
-                <p>Success</p>
-                <p>Partners</p>
-                <p>Resources</p>
-                <p>Company</p>
-            </nav>
+          <nav>
+            <span>braze</span>
+            <p>How it Works</p>
+            <p>Product</p>
+            <p>Solutions</p>
+            <p>Customers</p>
+            <p>Success</p>
+            <p>Partners</p>
+            <p>Resources</p>
+            <p>Company</p>
+          </nav>
         </div>
         <div className='right'>
-            <select name="" id="">
-                <option value="us"> US</option>
-            </select>
-            <button id='login'>Login</button>
-            <button id='get_start'>Get Started</button>
+          <select name='' id=''>
+            <option value='us'>US</option>
+          </select>
+          <button id='login'>Login</button>
+          <button id='get_start'>Get Started</button>
         </div>
       </header>
 
-    {/* header end */}
-    
+      {/* header end */}
+
       <div className='main'>
         <div className='text_con'>
-            <h1>There's never been a better time to be a better marker.<span>TM</span></h1>
-            <p>Data is flowing and channels are growing. Customers are demanding the world, while AI already transfroming it. Manage it all with the right <span>customer engagement platform.</span></p>
-            <div className='btns'>
-                <button>Get Started</button>
-                <button>Watch Video</button>
-            </div>
+          <h1>
+            There's never been a better time to be a better marker.
+            <span>TM</span>
+          </h1>
+          <p>
+            Data is flowing and channels are growing. Customers are demanding the
+            world, while AI already transforming it. Manage it all with the right{' '}
+            <span>customer engagement platform.</span>
+          </p>
+          <div className='btns'>
+            <button>Get Started</button>
+            <button>Watch Video</button>
+          </div>
         </div>
         <div className='image_con'>
-            <img src={Pic} alt="" />
+          <img src={Pic} alt='' />
         </div>
       </div>
-      
+
       {/* main end */}
 
       <div className='banner'>
         <h3>MADE TO SCALE WITH COMPANIES OF ALL SIZES</h3>
         <div className='icon_con'>
-            <div className='icons'>
-                <img src={Elf} alt="" />
-                <img src={Int} alt="" />
-                <img src={Go} alt="" />
-                <img src={Ro} alt="" />
-                <img src={Gar} alt="" />
-                <img src={Bl} alt="" />
-                <img src={St} alt="" />
-                <img src={Gy} alt="" />
-                <img src={Ch} alt="" />
+          <div className='icons'>
+            <img src={Elf} alt='' />
+            <img src={Int} alt='' />
+            <img src={Go} alt='' />
+            <img src={Ro} alt='' />
+            <img src={Gar} alt='' />
+            <img src={Bl} alt='' />
+            <img src={St} alt='' />
+            <img src={Gy} alt='' />
+            <img src={Ch} alt='' />
 
-                {/* Duplicate set of images */}
-                
-                <img src={Elf} alt="" />
-                <img src={Int} alt="" />
-                <img src={Go} alt="" />
-                <img src={Ro} alt="" />
-                <img src={Gar} alt="" />
-                <img src={Bl} alt="" />
-                <img src={St} alt="" />
-                <img src={Gy} alt="" />
-                <img src={Ch} alt="" />
-            </div>
-            
+            {/* Duplicate set of images */}
+
+            <img src={Elf} alt='' />
+            <img src={Int} alt='' />
+            <img src={Go} alt='' />
+            <img src={Ro} alt='' />
+            <img src={Gar} alt='' />
+            <img src={Bl} alt='' />
+            <img src={St} alt='' />
+            <img src={Gy} alt='' />
+            <img src={Ch} alt='' />
+          </div>
         </div>
         <button className='connect' onClick={handleConnectWithSalesClick}>
-            Connect With Sales
+          Connect With Sales
         </button>
       </div>
-      
 
       {isFormVisible && <PopupForm closeForm={closeForm} />}
-
     </DIV>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
 
 const DIV = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
   header {
     width: 80%;
     background-color: white;
     border-radius: 50px;
-    margin: auto; 
+    margin: auto;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 1rem; 
+    padding: 1rem;
     position: relative;
     top: 32px;
   }
@@ -128,26 +472,26 @@ const DIV = styled.div`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    gap: 1rem; 
+    gap: 1rem;
     font-weight: bolder;
   }
 
   p {
-    font-size: 0.75rem; 
+    font-size: 0.75rem;
   }
 
   nav > span {
     font-family: Cedarville Cursive;
-    font-size: 1.5rem; 
+    font-size: 1.5rem;
   }
 
   select {
-    margin-right: 0.5rem; 
+    margin-right: 0.5rem;
     background-color: whitesmoke;
   }
 
   #login {
-    margin-right: 0.5rem; 
+    margin-right: 0.5rem;
     background: none;
     border: none;
     font-weight: bold;
@@ -155,11 +499,11 @@ const DIV = styled.div`
   }
 
   #get_start {
-    margin-right: 0.5rem; 
+    margin-right: 0.5rem;
     border-radius: 50px;
     border: none;
-    padding: 0.75rem 1rem; 
-    background-color: #4E0BE0;
+    padding: 0.75rem 1rem;
+    background-color: #4e0be0;
     color: white;
     font-weight: bold;
     cursor: pointer;
@@ -168,17 +512,17 @@ const DIV = styled.div`
   .main {
     display: flex;
     width: 80%;
-    margin: 2rem auto; 
+    margin: 2rem auto;
     color: white;
-    min-height: 55vh;
+    flex-grow: 1;
   }
 
   .text_con {
     width: 50%;
     display: flex;
     flex-direction: column;
-    gap: 1rem; 
-    padding-top: 2rem; 
+    gap: 1rem;
+    padding-top: 2rem;
   }
 
   .text_con > h1 {
@@ -186,15 +530,15 @@ const DIV = styled.div`
   }
 
   .text_con > h1 > span {
-    font-size: 0.5rem; 
+    font-size: 0.5rem;
     position: relative;
-    top: -0.5rem; 
+    top: -0.5rem;
   }
 
   .text_con > p {
-    font-size: 1rem; 
+    font-size: 1rem;
     color: #e7dede;
-    margin-top: -1rem; 
+    margin-top: -1rem;
   }
 
   .text_con > p > span {
@@ -209,21 +553,20 @@ const DIV = styled.div`
   .image_con > img {
     width: 100%;
     max-width: 550px;
-  
-    height: auto; 
+    height: auto;
   }
 
   .btns {
     display: flex;
-    gap: 1rem; 
+    gap: 1rem;
   }
 
   .btns > button:nth-child(1) {
     border-radius: 50px;
     border: none;
-    padding: 0.75rem 1rem; 
+    padding: 0.75rem 1rem;
     background-color: white;
-    color: #8136CF;
+    color: #8136cf;
     font-weight: bold;
     cursor: pointer;
   }
@@ -231,7 +574,7 @@ const DIV = styled.div`
   .btns > button:nth-child(2) {
     border: none;
     background: transparent;
-    padding: 0.75rem 1rem; 
+    padding: 0.75rem 1rem;
     color: white;
     font-weight: bold;
     cursor: pointer;
@@ -241,16 +584,15 @@ const DIV = styled.div`
     background-color: white;
     border-radius: 70px 70px 0 0;
     text-align: center;
-    padding-top: 1rem; 
+    padding-top: 1rem;
     min-height: 220px;
     margin-top: -35px;
-   
-
+    flex-shrink: 0;
   }
 
   .banner > h3 {
     color: gray;
-    margin-bottom: 2rem; 
+    margin-bottom: 2rem;
   }
 
   .icon_con {
@@ -262,7 +604,7 @@ const DIV = styled.div`
 
   .icons {
     display: flex;
-    gap: 0 2rem; 
+    gap: 0 2rem;
     animation: slide 18s linear infinite;
   }
 
@@ -277,15 +619,15 @@ const DIV = styled.div`
 
   .connect {
     position: relative;
-    top: -2.5rem; 
-    left: 50%; 
-    transform: translateX(-50%); 
+    top: -2.5rem;
+    left: 50%;
+    transform: translateX(-50%);
     width: auto;
     max-width: 250px;
     border-radius: 50px;
     border: none;
-    padding: 0.75rem 1rem; 
-    background-color: #4E0BE0;
+    padding: 0.75rem 1rem;
+    background-color: #4e0be0;
     color: white;
     font-weight: bold;
     cursor: pointer;
@@ -307,10 +649,7 @@ const DIV = styled.div`
 
 
 
-
-
-
-
+///////////////
 
 
 
